f95ef2561b05f97ebf66188e2f7aa7c3c70b6cc5
Danny Aw <kokwah07@hotmail.com>
2017-12-18 18:27:13 +0800

    Changed the data from char to void type

--- src/Data.c
+++ src/Data.c
@@ -3,7 +3,7 @@
 #include <stdint.h>
 #include <stdio.h>
 
-Data *dataCreate(uint32_t key,char *name){
+Data *dataCreate(uint32_t key,void *name){
   Data *data = (Data *)malloc(sizeof(Data));
   data->key = key;
   data->name = name;
--- src/Data.h
+++ src/Data.h
@@ -10,5 +10,5 @@ struct Data{
 	void *name;
 };
 
-Data *dataCreate(uint32_t key,char *name);
+Data *dataCreate(uint32_t key,void *name);
 #endif // _DATA_H
